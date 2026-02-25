45a5925609550820272912fc69767023e5d08a65
Gabriel <gsotelosil14@gmail.com>
2026-02-24 17:44:32 -0800

    Changed aead.py to use pynacl since cryptography hazmat no longer has XChaCha20poly1305. Updated requirements.txt to reflect this. Added new entry points for server along with updated login and register.

--- crytpo/aead.py
+++ crytpo/aead.py
@@ -1,15 +1,16 @@
 import os
-from cryptography.hazmat.primitives.ciphers.aead import XChaCha20Poly1305
+from nacl.bindings import (
+    crypto_aead_xchacha20poly1305_ietf_encrypt,
+    crypto_aead_xchacha20poly1305_ietf_decrypt
+)
 
 def encrypt_plaintext(plain_text: bytes, derived_key: bytes) -> tuple[bytes, bytes]:
-    chacha = XChaCha20Poly1305(derived_key)
     nonce = os.urandom(24)
-    cipher_text = chacha.encrypt(nonce, plain_text, None)
+    cipher_text = crypto_aead_xchacha20poly1305_ietf_encrypt(plain_text, None, nonce, derived_key)
     return nonce, cipher_text
 
 
 def decrypt_ciphertext(cipher_text: bytes, nonce: bytes, derived_key: bytes) -> bytes:
     """Decrypt AEAD ciphertext. Raises if tag verification fails (tampered or wrong key)."""
-    chacha = XChaCha20Poly1305(derived_key)
-    return chacha.decrypt(nonce, cipher_text, None)
+    return crypto_aead_xchacha20poly1305_ietf_decrypt(cipher_text, None, nonce, derived_key)
 
--- server/main.py
+++ server/main.py
@@ -2,14 +2,30 @@ from fastapi import FastAPI, HTTPException, Depends
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from typing import List, Dict
+import os
+import base64
 from client.utilities.database import SessionLocal, User
+from crytpo.protocol import receive_message, MessagePayload
+from cryptography.hazmat.primitives.asymmetric import x25519
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ed25519
 
 app = FastAPI()
+SERVER_KEY_FILE = "server_dh_key.dat"
 
 class UserRegister(BaseModel):
     username: str
     display_name: str
-    pub_key: str
+    id_pub_key: str
+    dh_pub_key: str
+
+# Mirrors MessagePayload
+class UserLogin(BaseModel):
+    ciphertext: bytes | str
+    nonce: bytes | str
+    signature: bytes | str
+    sender_id: str
+    recipient_id: str
 
 class CreateGroup(BaseModel):
     creator: str
@@ -22,9 +38,46 @@ def get_db():
     finally:
         db.close()
 
+def get_server_key() -> x25519.X25519PrivateKey:
+    if os.path.exists(SERVER_KEY_FILE):
+        with open(SERVER_KEY_FILE, "rb") as file:
+            priv_bytes = file.read()
+            return x25519.X25519PrivateKey.from_private_bytes(priv_bytes)
+    else:
+        priv_key = x25519.X25519PrivateKey.generate()
+        with open (SERVER_KEY_FILE, "wb") as file:
+            file.write(priv_key.private_bytes(
+                encoding=serialization.Encoding.Raw,
+                format=serialization.PrivateFormat.Raw,
+                encryption_algorithm=serialization.NoEncryption()
+            ))
+        return priv_key
+
+def get_client_pub_keys(db: Session, username: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        raise ValueError("User not found in database")
+    id_bytes = bytes.fromhex(user.id_pub_key)
+    dh_bytes = bytes.fromhex(user.dh_pub_key)
+    id_public = ed25519.Ed25519PublicKey.from_public_bytes(id_bytes)
+    dh_public = x25519.X25519PublicKey.from_public_bytes(dh_bytes)
+    return id_public, dh_public
+
+server_dh_private = get_server_key()
+server_dh_public = server_dh_private.public_key()
+
 @app.get("/")
 def read_root():
     return {"status": "Chat Server Online"}
+
+@app.get("/pub_key")
+def get_public_key():
+    pub_key = server_dh_private.public_key()
+    pub_bytes = pub_key.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw
+    )
+    return {"server_dh_public": pub_bytes.hex()}
 
 @app.post("/register")
 async def register_user(user: UserRegister, db: Session = Depends(get_db)):
@@ -33,11 +86,43 @@ async def register_user(user: UserRegister, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Username already exist")
     new_user = User(username=user.username,
         display_name=user.display_name,
-        pub_key=user.pub_key)
+        id_pub_key=user.id_pub_key,
+        dh_pub_key=user.dh_pub_key
+    )
     db.add(new_user)
     db.commit()
-    db.refresh(new_user)
     return {"message": f"User {user.username} has been registered"}
+
+@app.post("/login")
+def login_user(payload: UserLogin, db: Session = Depends(get_db)):
+    try:
+        try:
+            client_identity_public, client_dh_public, = get_client_pub_keys(db, payload.sender_id)
+        except ValueError:
+            raise HTTPException(status_code=404, detail="Sender not found in database.")
+        raw_payload: MessagePayload = {
+            "ciphertext": base64.b64decode(payload.ciphertext),
+            "nonce": base64.b64decode(payload.nonce),
+            "signature": base64.b64decode(payload.signature),
+            "sender_id": payload.sender_id,
+            "recipient_id": payload.recipient_id,
+        }
+        plaintext_bytes = receive_message(
+            payload=raw_payload,
+            receiver_dh_private=server_dh_private,
+            sender_identity_public=client_identity_public,
+            sender_dh_public=client_dh_public
+        )
+        plain_text = plaintext_bytes.decode('utf-8')
+        return {
+            "status": "success",
+            "message": "Login payload has been successfully decrypted",
+            "decrypted_data": plain_text
+        }
+    except HTTPException:
+        raise HTTPException(status_code=404, detail="Sender not found in database.")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Authentication or decryption failed: {str(e)}")
 
 @app.get("/search/{username}")
 async def search_user(username: str, db: Session = Depends(get_db)):
